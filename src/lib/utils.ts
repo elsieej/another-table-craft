466a754c7370de9e4c05e76748d953e3c20ff793
elsie <lngmtri@gmail.com>
2026-07-30 09:44:21 +0700

    test: deliberately break lint to verify CI gate (will be reverted)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,5 +2,6 @@ import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
+  const unusedCiVerificationVariable = 'deliberately unused, verifying CI fails on lint errors'
   return twMerge(clsx(inputs))
 }
